56db13d2919203401e35ed49bded45269b9ed985
fabianfoidl <Fab28ian!>
2019-01-12 22:36:37 +0100

    FIX: changed attributes for running on localhost

--- src/main/java/com/se/ue04/Constants.java
+++ src/main/java/com/se/ue04/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String START_HOTEL_NAME = "Hotel";
     public static final String START_AIRPORT = "2";
     public static final String START_AIRPORT_NAME = "Airport";
-    //public static final String APIURL = "http://localhost:8080/api";
-    public static final String APIURL = "http://seue4.fhost.org/ue04/api";
+    public static final String APIURL = "http://localhost:8080/api";
+    //public static final String APIURL = "http://seue4.fhost.org/ue04/api";
     public static final String STANDARD_USER = "ROLE_USER";
 }
